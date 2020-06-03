1f23d6de116c3af286059f8a2646877a54d61cfd
Kevin Kim <kevinfkim@gmail.com>
2020-06-03 13:39:11 +0000

    complete the timer.

--- timer1.js
+++ timer1.js
@@ -1,20 +1,32 @@
-let timer = function() {
+const timer = function(argv) {
 
+  let input = process.argv.splice(2).sort((a, b) => a - b).map(x => x * 1000);
+  let times = [];
+  let aSecondaryTimer = 0;
 
-
-
-
-  let x = process.argv.splice(2).sort((a, b) => a - b);
-  console.log(x);
-
-  x.forEach(timer => {
-
+  input.forEach(t => {
+    if (t > 0) {
+      times.push(t);
+    }
   });
 
+  if (times === []) {
+    return;
+  } else {
 
+    for (time of times) {
 
+      setTimeout(() => {
+        process.stdout.write("."); // couldn't get system to beep, changing to printing a dot
+      }, time);
 
+      aSecondaryTimer = time;
 
+    }
+    setTimeout(() => {
+      process.stdout.write("\n");
+    }, aSecondaryTimer + 50);
 
-
-  module.exports = timer;
+  };
+};
+timer();
