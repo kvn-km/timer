2a25a9c413d5901d1631a7fa6586eb97baaed70f
Kevin Kim <kevinfkim@gmail.com>
2020-06-03 00:54:36 +0000

    first commit. setting up repo.

---
+++ timer1.js
@@ -0,0 +1,20 @@
+let timer = function() {
+
+
+
+
+
+  let x = process.argv.splice(2).sort((a, b) => a - b);
+  console.log(x);
+
+  x.forEach(timer => {
+
+  });
+
+
+
+
+
+
+
+  module.exports = timer;
